69890b50b82b5085ddbd50e60685347c0cac9d3c
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-20 11:35:24 +0000

    Updates

---
+++ precache-manifest.a235a02777ff0603bc80209c9052e41f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "09480accf77316ad56ffb85779c9f5e5",
+    "url": "/8-days-a-week/static/media/posture.09480acc.png"
+  },
+  {
+    "revision": "4e510d12c2203c00ba7481ea5e9919eb",
+    "url": "/8-days-a-week/static/media/person.4e510d12.png"
+  },
+  {
+    "revision": "637e82e40ec2128bccd06109ffde49d2",
+    "url": "/8-days-a-week/static/media/hourcode2016_girllllllllll.637e82e4.png"
+  },
+  {
+    "revision": "e74923f4d1ad3dfdf1739ed10fa361e0",
+    "url": "/8-days-a-week/static/media/food.e74923f4.png"
+  },
+  {
+    "revision": "edff7cdd14afdc5d8fa2",
+    "url": "/8-days-a-week/static/js/runtime~main.edff7cdd.js"
+  },
+  {
+    "revision": "d8f306191ef504fd3f01",
+    "url": "/8-days-a-week/static/js/main.d8f30619.chunk.js"
+  },
+  {
+    "revision": "fecd66d0feb43bdd5d47",
+    "url": "/8-days-a-week/static/js/1.fecd66d0.chunk.js"
+  },
+  {
+    "revision": "d8f306191ef504fd3f01",
+    "url": "/8-days-a-week/static/css/main.f3c9de20.chunk.css"
+  },
+  {
+    "revision": "fecd66d0feb43bdd5d47",
+    "url": "/8-days-a-week/static/css/1.8444ad82.chunk.css"
+  },
+  {
+    "revision": "16d21d5dd95b2cbd7425be848348eeb3",
+    "url": "/8-days-a-week/index.html"
+  }
+];
